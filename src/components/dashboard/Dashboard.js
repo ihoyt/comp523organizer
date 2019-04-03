39c5e9e2dd2f2db3c19dcbc54986764c1184d37f
Ian Hoyt <i_hoyt@yahoo.com>
2019-04-03 11:12:20 -0400

    Added react-select component to dashboard, not connected to state yet

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,8 +3,12 @@ import ProposalList from '../proposal/ProposalList';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
+import Select from 'react-select';
 
 class Dashboard extends Component {
+  state = {
+    filter: null
+  }
 
   getProposalsByCategory = (category) => {
     const { proposals } = this.props;
@@ -16,9 +20,35 @@ class Dashboard extends Component {
     }
   }
 
+  getSemesterChoices = () => {
+    const { proposals } = this.props;
+    let choices = [{ value: 'all', label: 'All'}];
+    let distinct = []; // Used to track if semester has already been added as option
+
+    if (proposals) {
+      proposals.forEach(function(proposal) {
+        if (!distinct.includes(proposal.semeseter) && proposal.semeseter !== ""
+              && typeof proposal.semeseter !== "undefined") {
+          distinct.push(proposal.semeseter);
+          choices.push({ value: proposal.semeseter, label: proposal.semeseter });
+        }
+      });
+    }
+
+    return choices;
+  }
+
   render() {
+    let semesters = this.getSemesterChoices();
+
     return(
       <div className="dashboard">
+        <div className="container filter-container">
+          <Select
+            defaultValue={ {value: 'all', label: "All"} }
+            options={semesters}
+          />
+        </div>
         <h3 className="roboto-font center dashboard-h">New</h3>
         <div className="container">
           <ProposalList proposals={this.getProposalsByCategory(0)} />
